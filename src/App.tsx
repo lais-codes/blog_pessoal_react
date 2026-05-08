d50ad7226aefc43c48668304a3f35e547aefd828
Lais Sousa <lais.b.sousa@outlook.com>
2026-05-08 12:48:33 -0300

    feat: implementação da lógica de login e autenticação de usuário

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,24 @@ import NavBar from './components/navbar/Navbar';
 import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro';
 import Login from './pages/login/Login';
+import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <NavBar />
-        <div className="min-h[80vh]"> 
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-          </Routes>
-        </div>
-        <Footer/>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <NavBar />
+          <div className="min-h[80vh]"> 
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+            </Routes>
+          </div>
+          <Footer/>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,21 +1,36 @@
-import { Link } from "react-router-dom";
-
-function NavBar() {
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/AuthContext";
+ 
+function Navbar() {
+ 
+    const navigate = useNavigate();
+ 
+    const { handleLogout } = useContext(AuthContext)
+ 
+    function logout() {
+ 
+        handleLogout()
+        alert('O Usuário foi desconectado com sucesso!')
+        navigate('/')
+    }
+ 
     return (
-        <>
-            <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
-                <div className="container flex justify-between text-lg mx-8">
-                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
-                    <div className=" flex gap-4">
-                        Postagens 
-                        Temas 
+    <>
+        <div className='w-full flex justify-center py-4 bg-indigo-900 text-white'>
+            <div className="container flex justify-between text-lg mx-8">
+                <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
                         Cadastrar tema
-                        Perfil 
-                        Sair
+                        Perfil
+                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
                     </div>
-                </div>
             </div>
-        </>
-    );
+        </div>
+    </>
+    )
 }
-export default NavBar;
+ 
+export default Navbar
